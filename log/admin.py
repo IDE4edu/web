0716e5f41be4bdd5a10e2847016b20e46b4f36f4
Nathaniel Titterton <nate@berkeley.edu>
2014-03-28 13:03:31 -0700

    fixing reference to verb

--- log/admin.py
+++ log/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from log.models import *
 
 class LogAdmin(admin.ModelAdmin):
-    list_display = ('subject', 'verb', 'time')
+    list_display = ('subject', 'action', 'time')
 
 
 admin.site.register(ActivityLog, LogAdmin)
